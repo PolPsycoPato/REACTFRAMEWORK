a6100cff14a3c00c8d49891968ccbccbab3271af
luismi937 <luismicanizares02@gmail.com>
2025-10-14 14:24:00 +0200

    DEPORTES

--- reactclases/src/index.js
+++ reactclases/src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import DibujosComplejos from './components/DibujosComplejos'
 import DibujosComplejosRender from './components/DibujosComplejosRender';
+import PadreDeportes from './components/PadreDeportes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <DibujosComplejosRender/>
+   <PadreDeportes />
   </React.StrictMode>
 );
 
